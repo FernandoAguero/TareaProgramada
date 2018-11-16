014cabc61289fb547ba8f6be9958c9881cae055a
FernandoAguero <44984383+FernandoAguero@users.noreply.github.com>
2018-11-16 15:55:18 -0600

    sets y gets en atributos de neuronas, junto con asignaciones random

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include "neurona.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Neurona n;
+    n.imprimirCarga();
+
 
     return a.exec();
 }
--- neurona.h
+++ neurona.h
@@ -1,6 +1,11 @@
 #ifndef NEURONA_H
 #define NEURONA_H
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 
 class Conexion;
@@ -14,6 +19,37 @@ class Neurona{
     vector<Conexion> listaConexiones;
 
 public:
+    Neurona(){
+        srand (static_cast <unsigned> (time(0)));
+        carga=static_cast <float> (rand()) / static_cast <float> (RAND_MAX);;
+        umbral=static_cast <float> (rand()) / static_cast <float> (RAND_MAX);;
+    }
+    float getCarga(){
+        return carga;
+    }
+
+    void imprimirCarga(){
+            ostringstream buff;
+            buff<<carga;
+            cout<<buff.str()<<endl;
+
+    }
+    void setCarga(float k){
+        carga=k;
+    }
+    float getUmbral(){
+        return umbral;
+    }
+
+    void imprimirUmbral(){
+            ostringstream buff;
+            buff<<umbral;
+            cout<<buff.str()<<endl;
+
+    }
+    void setUmbral(float k){
+        umbral=k;
+    }
 
     void AgregarConexion(Neurona n, float peso){
         Conexion c = Conexion(n, peso);
