230277ecf74a8f7f44bc1c27422bbaa32d40def1
FernandoAguero <44984383+FernandoAguero@users.noreply.github.com>
2018-11-25 14:21:59 -0600

    Algunos nuevos metodos
    Trato de modificar cargas pero faltan algunas condiciones que no tengo claras.

--- main.cpp
+++ main.cpp
@@ -11,23 +11,48 @@ int main(int argc, char *argv[])
     Red red1 = Red();
 
     red1.AgregarCapa(1);
-    red1.AgregarCapa(3);
-    red1.AgregarCapa(5);
-    red1.AgregarCapa(5);
-    red1.AgregarCapa(3);
+    red1.AgregarCapa(2);
+    red1.AgregarCapa(4);
+    red1.AgregarCapa(8);
     red1.AgregarCapa(1);
 
+
+
+    //red1.MostrarRed();
+
+    float valorEsperado=666;
+    float prueba= red1.Entrenar(valorEsperado);
+    while((red1.CalcularFallo(prueba,valorEsperado))<80){
+        valorEsperado=red1.CalcularFallo(prueba,valorEsperado);
+        red1.ModificarCargas(prueba);
+        prueba=red1.Entrenar(valorEsperado);
+
+     }
+
+    cout<<"el numero esperado era "<<prueba<<endl;
+
     red1.MostrarRed();
-    int c=0;
-    while(c<100){
-    cout << red1.Entrenar(c)<<endl;
-    c++;
+    /*int c=1;
+    int i=1;
+    float resultado=0.0;
+    while(i<2){
+    resultado=red1.Entrenar(c);
+    cout << resultado <<endl;
+    if(resultado!=c){
+        red1.Entrenar(resultado);
+
+        //cout<<"el porcentaje de fallo es "<< red1.CalcularFallo(red1.Entrenar(resultado),c);
+
     }
+    i++;
 
+    // falta como modificar las cargas y pesos y lo de if carga>umbral
+    }
+    cout<<"0.4 es tanto % de 2 "<<red1.CalcularFallo(0.4,2.0)<<endl;
     red1.MostrarRed();
 
 
-    /*Neurona n;
+    Neurona n;
     Neurona m;
     Neurona k;
     n.imprimirCarga();
